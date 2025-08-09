cc2c9084d24fb1b2414023ac29e73762e593d053
Lucas Gabriel <lucas@solidstackcloud.com>
2025-08-09 13:55:37 -0300

    fix: data

--- data.tf
+++ data.tf
@@ -2,3 +2,11 @@ data "aws_ssm_parameter" "pods_subnet" {
   count = var.solidstack_vpc_module ? 1 : 0
   name  = "/${var.project_name}/pods-subnet-ids"
 }
+
+data "aws_eks_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = data.aws_eks_cluster.main.id
+}
